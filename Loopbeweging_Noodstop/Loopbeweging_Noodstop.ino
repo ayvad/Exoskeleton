683d3d23065599ddf291d437ac7799e260da1db3
Ayke Vaders <58824615+ayvad@users.noreply.github.com>
2022-03-29 14:54:33 +0200

    Added debugcode

--- Loopbeweging_Noodstop/Loopbeweging_Noodstop.ino
+++ Loopbeweging_Noodstop/Loopbeweging_Noodstop.ino
@@ -17,6 +17,7 @@ const uint8_t R_EN = 7;
 const uint8_t L_EN = 8;
 const uint8_t L_PWM = 5;
 const uint8_t R_PWM = 6;
+const int pinEmergencyBrake = 2;
 
 BTS7960 motorController(L_EN, R_EN, L_PWM, R_PWM);
 
@@ -26,13 +27,22 @@ int angleZ = 0;
 float x, y, z;
 int anglePos = 0;
 int angleNeg = 0;
-int beweging = 0;
+int movement;
+enum movementDirection {up = 1, down = -1, still = 0};
 int lastAngle;
+
+void emergency(){
+  motorController.Stop();
+  motorController.Disable();
+}
+
 
 void setup() {
   Serial.begin(9600);
   IMU.begin();
   motorController.Enable();
+  pinMode(pinEmergencyBrake, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinEmergencyBrake), emergency, CHANGE);
 }
 
 void loop() {
@@ -51,20 +61,20 @@ void loop() {
     angleNeg = angleZ + FOUTMARGE;
 
     if (angleNeg < 0) {
-      beweging = -1;
+      movement = down;
       Serial.println("beweging: -1");
     }
     else if (anglePos > 0) {
-      beweging = 1;
+      movement = up;
       Serial.println("beweging: 1");
     }
     else {
-      beweging = 0;
+      movement = still;
       Serial.println("beweging: 0");
     }
   }
   // loopbeweging
-  if (beweging == 1) {
+  if (movement == up) {
     if (potAngle > lastAngle) {
       if ((HOEK1 - potAngle) < REMMARGE) {
         // Eerste boog omhoog snel
@@ -90,7 +100,7 @@ void loop() {
         }
       }
     }
-    if (beweging == -1) {
+    if (movement == down) {
       if (potAngle > lastAngle) {
         if ((HOEK3 - potAngle) < REMMARGE) {
           //Tweede boog omhoog snel
